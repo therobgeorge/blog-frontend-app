0d50c11d1ad9448a41d912e902a1fcf4215d522c
therobgeorge <georg1rd@gmail.com>
2021-06-21 17:28:14 -0400

    adds bootstrap and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Blogs!</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/posts">Posts</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/posts/new">New Post</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/signup">Signup</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/login">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/logout">Logout</router-link>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/posts">Posts</router-link>
-      |
-      <router-link to="/posts/new">New Post</router-link>
-      |
-      <router-link to="/about">About</router-link>
-      |
-      <router-link to="/test">Test</router-link>
       |
       <span v-if="!isLoggedIn()">
         <router-link to="/signup">Signup</router-link>
@@ -28,29 +61,6 @@
     </div>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
 
 <script>
 export default {
--- src/views/posts/Index.vue
+++ src/views/posts/Index.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="posts-index">
-    <input type="text" v-model="searchTerm" placeholder="Search" />
-    <div v-for="post in filterBy(posts, searchTerm, 'title')" v-bind:key="post.id">
-      <router-link :to="`posts/${post.id}`">
-        <h2>{{ post.title }}</h2>
-      </router-link>
-      <img :src="post.image" alt="" />
-      <p>{{ post.body }}</p>
-      <p>Created: {{ relativeDate(post.created_at) }}</p>
+    <input type="text" v-model="searchTerm" list="titles" placeholder="Search" />
+    <datalist id="titles">
+      <option v-for="post in posts" v-bind:key="post.id">
+        {{ post.title }}
+      </option>
+    </datalist>
+    <br />
+    <br />
+    <button v-on:click="setSortAttribute('title')">Sort by title</button>
+    <button v-on:click="setSortAttribute('created_at')">Sort by date</button>
+
+    <div
+      is="transition-group"
+      appear
+      enter-active-class="animate__animated animate__fadeIn"
+      leave-active-class="animate__animated animate__fadeOut"
+    >
+      <div v-for="post in filterBy(orderBy(posts, sortAttribute), searchTerm, 'title')" v-bind:key="post.id">
+        <router-link :to="`posts/${post.id}`">
+          <h2>{{ post.title }}</h2>
+        </router-link>
+        <img :src="post.image" alt="" />
+        <p>{{ post.body }}</p>
+        <p>Created: {{ relativeDate(post.created_at) }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -24,6 +41,7 @@ export default {
     return {
       posts: [],
       searchTerm: "",
+      sortAttribute: "",
     };
   },
   created: function () {
@@ -35,6 +53,9 @@ export default {
   methods: {
     relativeDate: function (date) {
       return moment(date).fromNow();
+    },
+    setSortAttribute: function (attribute) {
+      this.sortAttribute = attribute;
     },
   },
 };
